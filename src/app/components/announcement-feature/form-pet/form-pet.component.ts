2e0c9a6b7109d5e1b6f8f7be3ac339231857f72a
jpaomat@gmail.com <jpaomat@gmail.com>
2019-12-16 11:21:46 -0500

    update

--- src/app/components/announcement-feature/form-pet/form-pet.component.ts
+++ src/app/components/announcement-feature/form-pet/form-pet.component.ts
@@ -14,19 +14,59 @@ export class FormPetComponent implements OnInit {
   get tituloPost(){
     return this.registerForm.get('tituloPost');
   }
-
+  get typePet(){
+    return this.registerForm.get('typePet');
+  }
+  get namePet(){
+    return this.registerForm.get('namePet');
+  }
+  get racePet(){
+    return this.registerForm.get('racePet');
+  }
+  get agePet(){
+    return this.registerForm.get('agePet');
+  }
+  get generoPet(){
+    return this.registerForm.get('generoPet');
+  }
+  get vaccinePet(){
+    return this.registerForm.get('vaccinePet');
+  }
+  get sterilizePet(){
+    return this.registerForm.get('sterilizePet');
+  }
+  get locatedPet(){
+    return this.registerForm.get('locatedPet');
+  }
+  get email(){
+    return this.registerForm.get('email');
+  }
+  get celular(){
+    return this.registerForm.get('celular');
+  }
+  
   registerForm= this.formBuilder.group({
-    tituloPost:['', Validators.required], 
-    typePet:[''],
-    namePet:[''],
-    racePet:[''],
-    agePet:[''],
-    generoPet:[''],
-    vaccinePet:[''],
-    sterilizePet:[''],
-    locatedPet:[''],
-    email:[''],
-    celular:[''],
+    tituloPost:['',{
+      validators:[Validators.required,Validators.pattern("[a-zA-ZñÑáéíóúÁÉÍÓÚ/s]+")]
+    }], 
+    typePet:['',Validators.required],
+    namePet:['',{
+      validators:[Validators.required,Validators.pattern("[a-zA-ZñÑáéíóúÁÉÍÓÚ/s]+")]
+    }],
+    racePet:['',{
+      validators:[Validators.required,Validators.pattern("[a-zA-ZñÑáéíóúÁÉÍÓÚ/s]+")]
+    }],
+    agePet:['',Validators.required],
+    generoPet:['',Validators.required],
+    vaccinePet:['',Validators.required],
+    sterilizePet:['',Validators.required],
+    locatedPet:['',Validators.required],
+    email:['',
+      [Validators.required, Validators.email,Validators.pattern("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")]
+    ],
+    celular:['',{
+      validators:[ Validators.required, Validators.minLength(10), Validators.maxLength(10)]
+    }],
   })
 
   ngOnInit() {
